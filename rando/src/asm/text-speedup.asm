bdb467cf6c4779d60f6258ae44ace915432680e7
Erik Gilling <konkers@konkers.net>
2020-07-07 16:51:10 -0700

    [rando] Add text-speedup.asm patch.

---
+++ rando/src/asm/text-speedup.asm
@@ -0,0 +1,10 @@
+arch pce.cpu
+
+origin $88f1;
+    jsr $896A
+    jsr $896A
+    jsr $896A
+    nop
+    nop
+done:
+    rts
--- rando/src/patches.rs
+++ rando/src/patches.rs
@@ -1,5 +1,8 @@
 use lazy_static::lazy_static;
 
 lazy_static! {
-    pub static ref PATCHES: Vec<&'static [u8]> = vec![];
+    pub static ref PATCHES: Vec<&'static [u8]> = vec![include_bytes!(concat!(
+        env!("OUT_DIR"),
+        "/asm/text-speedup.ips"
+    )),];
 }

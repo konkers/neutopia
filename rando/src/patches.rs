0b015a849f540b3482e8d17e083947dd7f953d5f
Erik Gilling <konkers@konkers.net>
2020-07-08 22:40:58 -0700

    [rando] Add patch to skip intro.

--- rando/src/patches.rs
+++ rando/src/patches.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref PATCHES: Vec<&'static [u8]> = vec![
+        include_bytes!(concat!(env!("OUT_DIR"), "/asm/intro-skip.ips")),
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/progressive-items.ips")),
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/text-speedup.ips")),
     ];

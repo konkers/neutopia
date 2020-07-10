3e259fbf0d06754dfad8c544aec9deae5d9a353a
Erik Gilling <konkers@konkers.net>
2020-07-09 18:20:02 -0700

    [neutil] Add command to generate checks list.

---
+++ neutil/src/checks.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+use failure::Error;
+use structopt::StructOpt;
+
+use neutopia::Neutopia;
+use rando::Check;
+
+#[derive(StructOpt, Debug)]
+pub(crate) struct ChecksOpt {
+    #[structopt(long, parse(from_os_str), default_value = "Neutopia (USA).pce")]
+    rom: PathBuf,
+
+    #[structopt(long, parse(from_os_str), default_value = "out")]
+    out: PathBuf,
+}
+
+pub(crate) fn command(opt: &ChecksOpt) -> Result<(), Error> {
+    let mut f = File::open(&opt.rom)?;
+    let mut data = Vec::new();
+    // read the whole file
+    f.read_to_end(&mut data)?;
+
+    let n = Neutopia::new(&data)?;
+
+    let chests = n.filter_chests(|chest| {
+        // All areas that are non the end game area.
+        (chest.area < 0x10)
+                // Chest does not contain medallion
+                && (chest.info.item_id < 0x12 || chest.info.item_id >= (0x12 + 8))
+    });
+
+    let mut checks = Vec::new();
+    for chest in &chests {
+        let name = format!(
+            "{} - {}",
+            neutopia::area_name(chest.area),
+            chest.info.get_item_name(),
+        );
+        let check = Check {
+            name,
+            area: chest.area,
+            room: chest.room,
+            gates: Vec::new(),
+        };
+        checks.push(check);
+    }
+
+    let f = File::create(&opt.out)?;
+    serde_json::to_writer_pretty(f, &checks)?;
+    Ok(())
+}
--- neutil/src/main.rs
+++ neutil/src/main.rs
@@ -1,11 +1,13 @@
 use failure::Error;
 use structopt::StructOpt;
 
+mod checks;
 mod doc;
 mod info;
 
 #[derive(StructOpt, Debug)]
 enum Opt {
+    Checks(checks::ChecksOpt),
     Doc(doc::DocOpt),
     Info(info::InfoOpt),
 }
@@ -13,6 +15,7 @@ enum Opt {
 fn main() -> Result<(), Error> {
     let opt = Opt::from_args();
     match &opt {
+        Opt::Checks(checks_opt) => checks::command(checks_opt),
         Opt::Doc(doc_opt) => doc::command(doc_opt),
         Opt::Info(info_opt) => info::command(info_opt),
     }

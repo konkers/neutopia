7d7e17e5965dd3aad1f528ed39284a6b24b3d85d
Erik Gilling <konkers@konkers.net>
2020-07-13 19:18:25 -0700

    [rando-web] Add rom upload and verify to web interface.

--- rando-web/src/lib.rs
+++ rando-web/src/lib.rs
@@ -1,25 +1,54 @@
+#![recursion_limit = "256"]
+
 use wasm_bindgen::prelude::*;
-use yew::prelude::*;
+use yew::services::reader::{File, FileData, ReaderService, ReaderTask};
+use yew::{html, prelude::*, ChangeData, Component, ComponentLink, Html, ShouldRender};
+
+use neutopia::verify;
 
 struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+
+    reader: ReaderService,
+    tasks: Vec<ReaderTask>,
+
+    verified_str: String,
 }
 
 enum Msg {
-    AddOne,
+    File(Option<File>),
+    Loaded(FileData),
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        Self {
+            reader: ReaderService::new(),
+            tasks: vec![],
+            verified_str: "".into(),
+            link,
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1,
+            Msg::File(file) => {
+                log::info!("test2");
+                if let Some(file) = file {
+                    let callback = self.link.callback(Msg::Loaded);
+                    let task = self.reader.read_file(file, callback).unwrap();
+                    // How does this get cleaned up?
+                    self.tasks.push(task);
+                }
+            }
+            Msg::Loaded(file) => {
+                self.verified_str = match verify(&file.content) {
+                    Ok(info) => format!("{:?}", &info),
+                    Err(e) => format!("invalid rom: {}", e),
+                }
+            }
         }
         true
     }
@@ -34,8 +63,21 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <input type="file" multiple=false onchange=self.link.callback(move |value| {
+                    let mut result = None;
+                    if let ChangeData::Files(files) = value {
+                        let file = js_sys::try_iter(&files)
+                            .unwrap()
+                            .unwrap()
+                            .into_iter()
+                            .map(|v| File::from(v.unwrap()))
+                            .next()
+                            .unwrap();
+                        result = Some(file);
+                    }
+                    Msg::File(result)
+                })/>
+                <p>{ &self.verified_str }</p>
             </div>
         }
     }
@@ -43,5 +85,6 @@ impl Component for Model {
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
+    wasm_logger::init(wasm_logger::Config::default());
     App::<Model>::new().mount_to_body();
 }

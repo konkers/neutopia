3c06a253d86a3c5cadccf62806ec142b818f50a7
Erik Gilling <konkers@konkers.net>
2020-07-14 23:22:05 -0700

    [rando] Add patch to open all spheres at the beginning.

--- rando/src/patches.rs
+++ rando/src/patches.rs
@@ -3,6 +3,7 @@ use lazy_static::lazy_static;
 lazy_static! {
     pub static ref PATCHES: Vec<&'static [u8]> = vec![
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/intro-skip.ips")),
+        include_bytes!(concat!(env!("OUT_DIR"), "/asm/open-stairs.ips")),
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/text-speedup.ips")),
     ];
 }

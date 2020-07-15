68312a5ef2a46ccfb486a34403ca5cc4b21edcf8
Erik Gilling <konkers@konkers.net>
2020-07-14 19:18:08 -0700

    [rando] Disable progressive items while it's being stabilized.

--- rando/src/patches.rs
+++ rando/src/patches.rs
@@ -3,7 +3,6 @@ use lazy_static::lazy_static;
 lazy_static! {
     pub static ref PATCHES: Vec<&'static [u8]> = vec![
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/intro-skip.ips")),
-        include_bytes!(concat!(env!("OUT_DIR"), "/asm/progressive-items.ips")),
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/text-speedup.ips")),
     ];
 }

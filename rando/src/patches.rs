770dbbc3dd6a61d418f38683f43ccfb729ea1039
Erik Gilling <konkers@konkers.net>
2020-07-17 01:15:00 -0700

    [rando] Add patch to gain 24 bits of save state.

--- rando/src/patches.rs
+++ rando/src/patches.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref PATCHES: Vec<&'static [u8]> = vec![
+        include_bytes!(concat!(env!("OUT_DIR"), "/asm/expand-save-state.ips")),
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/intro-skip.ips")),
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/no-downgrade.ips")),
         include_bytes!(concat!(env!("OUT_DIR"), "/asm/open-stairs.ips")),
